1ee2852d39a41873474cdeb6fcf91ac8f0a98846
Tim Förster <tim@foerster-technologies.com>
2020-04-03 15:15:09 +0200

    init

---
+++ src/main/java/secure/open/standard/SosBackendApplication.java
@@ -0,0 +1,21 @@
+package secure.open.standard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
+public class UserServiceApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+    return application.sources(UserServiceApplication.class);
+  }
+
+  public static void main(final String[] args) {
+    SpringApplication.run(UserServiceApplication.class, args);
+  }
+
+}
